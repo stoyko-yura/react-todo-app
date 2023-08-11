357ab1917158dff64760301b7d145343352976cd
stoyko-yura <stoyko.yuri@gmail.com>
2023-08-11 19:45:27 +0300

    feat: initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,85 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'airbnb',
+    'airbnb-base',
+    'airbnb/hooks',
+    'airbnb-typescript/base',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    '@typescript-eslint/no-shadow': 0,
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      { prefer: 'type-imports', disallowTypeAnnotations: false }
+    ],
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function'
+      }
+    ],
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/require-default-props': 0,
+    'react/button-has-type': 0,
+    'react/no-unstable-nested-components': [
+      2,
+      {
+        allowAsProps: true
+      }
+    ],
+    'react/no-array-index-key': 0,
+    'no-param-reassign': 0,
+    'sort-keys': [1, 'asc', { natural: true, allowLineSeparatedGroups: true }],
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'react/jsx-sort-props': [
+      2,
+      {
+        callbacksLast: true,
+        shorthandFirst: true,
+        multiline: 'last',
+        ignoreCase: true,
+        reservedFirst: true
+      }
+    ],
+    'import/order': 0,
+    'import/extensions': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/prefer-default-export': 0,
+    'import/export': 0,
+    'import/order': [
+      2,
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+        pathGroups: [
+          {
+            pattern: '@/**',
+            group: 'internal'
+          }
+        ],
+        alphabetize: {
+          order: 'asc'
+        }
+      }
+    ]
+  }
+};
